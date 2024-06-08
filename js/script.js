ace53c1ff52f3154d08462afc980693bb64cc125
ctaylor <hi.ctaylor.co@gmail.com>
2024-06-07 19:12:44 -0500

    Added JS code for expense tracker to display list, find min and max values and disable button

--- js/script.js
+++ js/script.js
@@ -3,26 +3,34 @@ const results = document.querySelector(".results");
 const expensesList = document.querySelector(".expenses");
 
 const expenses = [
-  9.99,
-  25.8,
-  144.39,
-  12.98,
-  4.99,
-  38.75,
-  14.5,
-  99.99,
-  75.4,
-  4.75,
-  62.88
+  9.99, 25.8, 144.39, 12.98, 4.99, 38.75, 14.5, 99.99, 75.4, 4.75, 62.88,
 ];
 
-// Display your expenses in a list
+//List is displayed on the screen. When user clicks the button, min and max items are displayed and button is disabled.
+
 for (let exp of expenses) {
   let li = document.createElement("li");
   li.innerText = `$ ${exp}`;
   expensesList.append(li);
 }
 
-// Write showMinMax function here
+const showMinMax = function (expenses) {
+  const min = Math.min(...expenses);
+  const max = Math.max(...expenses);
 
-// Write click event listener for 'button' and call the showMinMax function
+  const minLi = document.createElement("li");
+  minLi.innerText = `Min: $ ${min}`;
+
+  const maxLi = document.createElement("li");
+  maxLi.innerText = `Max: $ ${max}`;
+
+  results.append(minLi);
+  results.append(maxLi);
+
+  results.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+  showMinMax(expenses);
+  button.disabled = true;
+});
